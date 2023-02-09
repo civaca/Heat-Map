a162ac6ac3fa3f0c388a3fb1e23bcadd103c596d
Cristian Vaca <civaca7@gmail.com>
2023-02-08 23:45:52 -0500

    inital heat-map

---
+++ fun.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded",()=>{
+//fetchind data
+fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
+.then(response=>response.json())
+.then(data=>{ const dataset=data;
+
+const w=500;
+const h=500;
+const padding=80;
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//scale 
+const xScale=d3.scaleTime().domain([d3.min(dataset["monthlyVariance"],(d,i)=> d3.timeParse("%Y")(d["year"])),d3.max(dataset["monthlyVariance"],(d,i)=> d3.timeParse("%Y")(d["year"]))])
+.range([padding,w-padding])
+
+const yScale=d3.scaleTime().domain([d3.min(dataset["monthlyVariance"],(d,i)=>d3.timeParse("%m")(d["month"])),d3.max(dataset["monthlyVariance"],d=>d3.timeParse("%m")(d["month"]))])
+.range([padding,h-padding])
+//creating svg
+const svg=d3.select("body")
+        .append("svg")
+        .attr("width",w)
+        .attr("height",h)
+        
+
+        svg.selectAll("rect")
+        .data(dataset["monthlyVariance"])
+        .enter()
+        .append("rect")
+        .attr("x",(d,i)=>xScale(d3.timeParse("%Y")(d["year"])))
+        .attr("y",d=>yScale(d3.timeParse("%m")(d["month"])))
+        .attr("width",2)
+        .attr("height",d=>yScale(d3.timeParse("%m")(d["month"])))
+        .attr("fill","yellow")
+
+
+    //Axis
+
+    const xAxis=d3.axisBottom(xScale)
+     const yAxis=d3.axisLeft(yScale).tickFormat(d => monthNames[d.getMonth()])
+     
+
+    svg.append("g")
+        .attr("transform", "translate(0," + (h-padding) + ")")
+        .call(xAxis)
+        .attr("id","x-axis")
+
+        svg.append("g")
+        .attr("transform", "translate("+padding+",0)")
+        .call(yAxis)
+        .attr("id","y-axis")
+
+
+
+
+
+    })//Closing fetching
+})//closing loaded
